272aff9506481d8a43e1c8dd3dfe3d64120c6645
Yonle <yonle@proton.me>
2026-06-21 07:09:02 +0700

    keep retrying
    Signed-off-by: Yonle <yonle@proton.me>

--- relay.sh
+++ relay.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+trap 'kill -KILL -- -$$ 2>/dev/null' INT TERM KILL
+
 categ="$1"
 name="$2"
 url="$3"
@@ -13,28 +15,32 @@ source config.sh
 
 streamdir="$ROOT_STREAMDIR/$categ/$name"
 
-rm -rf "$streamdir/*.ts"
-mkdir -p "$streamdir"
+while true; do
+	rm -rf "$streamdir/*.ts"
+	mkdir -p "$streamdir"
 
-exec ffmpeg \
-  -readrate "$READRATE" \
-  -readrate_initial_burst "$READRATE_INITIAL_BURST" \
-  -readrate_catchup "$READRATE_CATCHUP" \
-  -analyzeduration 0 \
-  -hide_banner -loglevel info \
-  -i "$url" \
-  -c copy \
-  -f hls \
-  -rw_timeout 15000000 \
-  -reconnect 1 \
-  -reconnect_at_eof 1 \
-  -reconnect_streamed 1 \
-  -reconnect_on_network_error 1 \
-  -reconnect_delay_max 5 \
-  -hls_time "$HLS_TIME" \
-  -hls_list_size "$HLS_LIST_SIZE" \
-  -hls_flags delete_segments+append_list+temp_file \
-  -hls_delete_threshold "$HLS_DELETE_THRESHOLD" \
-  -hls_segment_filename "$streamdir/%06d.ts" \
-  -hls_allow_cache 0 \
-  "$streamdir/index.m3u8"
+	exec ffmpeg \
+	  -readrate "$READRATE" \
+	  -readrate_initial_burst "$READRATE_INITIAL_BURST" \
+	  -readrate_catchup "$READRATE_CATCHUP" \
+	  -analyzeduration 0 \
+	  -hide_banner -loglevel info \
+	  -i "$url" \
+	  -c copy \
+	  -f hls \
+	  -rw_timeout 15000000 \
+	  -reconnect 1 \
+	  -reconnect_at_eof 1 \
+	  -reconnect_streamed 1 \
+	  -reconnect_on_network_error 1 \
+	  -reconnect_delay_max 5 \
+	  -hls_time "$HLS_TIME" \
+	  -hls_list_size "$HLS_LIST_SIZE" \
+	  -hls_flags delete_segments+append_list+temp_file \
+	  -hls_delete_threshold "$HLS_DELETE_THRESHOLD" \
+	  -hls_segment_filename "$streamdir/%06d.ts" \
+	  -hls_allow_cache 0 \
+	  "$streamdir/index.m3u8"
+
+	sleep 5 # retry in 5 seconds.
+done
