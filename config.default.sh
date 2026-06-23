a2c55ec42cdd099ff5f2580092f4c7e73fcbf56e
Yonle <yonle@proton.me>
2026-06-24 04:02:48 +0700

    disable readrate by default
    Signed-off-by: Yonle <yonle@proton.me>

--- config.default.sh
+++ config.default.sh
@@ -18,19 +18,19 @@ FFMPEG_LOGLEVEL=info
 
 # upstream read rate
 # see ffmpeg manpage on -readrate
-READRATE=1
-READRATE_INITIAL_BURST=2
-READRATE_CATCHUP=2
+# READRATE=1
+# READRATE_INITIAL_BURST=2
+# READRATE_CATCHUP=2
 
 # If FFmpeg reports many packet corruption errors, but the issue cannot
 # be reproduced in a normal media player, the upstream server may expect
 # clients to consume the stream in real time instead of reading ahead
 # during startup.
 #
-# In that case, disable read rate limiting:
-# READRATE=0
-# READRATE_INITIAL_BURST=0
-# READRATE_CATCHUP=0
+# In that case, often times we just don't need readrate in real live stream:
+READRATE=0
+READRATE_INITIAL_BURST=0
+READRATE_CATCHUP=0
 
 # how long each segments should be?
 HLS_TIME=4
