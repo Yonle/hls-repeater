decae7352123e4e564520d72d9332eeeb934d798
Yonle <yonle@proton.me>
2026-06-27 00:23:35 +0700

    relay: make copytb options adjustable
    Signed-off-by: Yonle <yonle@proton.me>

--- relay.sh
+++ relay.sh
@@ -79,7 +79,7 @@ CMD+=(
 	-c copy
 )
 
-[ "$FFMPEG_COPYTB" == "1" ] && CMD+=(-copytb 1)
+! [ -z "$FFMPEG_COPYTB" ] && [ "$FFMPEG_COPYTB" != "0" ] && CMD+=(-copytb $FFMPEG_COPYTB)
 [ "$FFMPEG_COPYTS" == "1" ] && CMD+=(-copyts)
 [ "$FFMPEG_TS_START_AT_ZERO" == "1" ] && CMD+=(-start_at_zero)
 
