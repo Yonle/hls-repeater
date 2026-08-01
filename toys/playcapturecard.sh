6a9e657511719cb167069e6116fb531fe5d943c6
Yonle <yonle@proton.me>
2026-08-02 04:41:10 +0700

    nene~
    Signed-off-by: Yonle <yonle@proton.me>

---
+++ toys/playcapturecard.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+vid_in="$1"
+aud_in="$2"
+
+if [ -z "$vid_in" ] || [ -z "$aud_in" ]; then
+cat <<-EOF
+Usage: ./toys/playcapturecard.sh <v4l2device> <pipewiresink>
+
+Example:
+  ./playcapturecard.sh /dev/video2 alsa_input.usb-MACROSILICON_2109-02.analog-stereo
+
+To get your pipewire sink, Please do the following:
+  pw-cli ls | grep -i node.name
+EOF
+
+exit 1
+fi
+
+pw-cat \
+  --target "${aud_in}" \
+  -r - | \
+    pw-cat \
+      --latency=384 \
+      -p - &
+
+
+trap 'kill -KILL -- -$$ 2>/dev/null' INT TERM EXIT
+mpv \
+  --hwdec=qsv \
+  --vd=mjpeg_qsv \
+  --profile=low-latency \
+  --cache=no \
+  --demuxer-lavf-format=v4l2 \
+  --demuxer-lavf-o=input_format=mjpeg \
+  --video-sync=display-vdrop \
+  --demuxer-max-bytes=64KiB \
+  --demuxer-max-back-bytes=0 \
+  "av://v4l2:${vid_in}"
