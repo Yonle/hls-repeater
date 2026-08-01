1d91c8be938ff0354eb670fc52ac1285c86ac1ff
Yonle <yonle@proton.me>
2026-08-01 21:48:47 +0700

    support for different container
    Signed-off-by: Yonle <yonle@proton.me>

--- toys/capturecard.sh
+++ toys/capturecard.sh
@@ -3,9 +3,11 @@
 vid_in="$1"
 aud_in="$2"
 out="$3"
+cont="${4:-nut}"
 
 if [ -z "$vid_in" ] || [ -z "$aud_in" ] || [ -z "$out" ]; then
-  echo "Usage: ./toys/capturecard.sh /dev/videoX hw:1,0 [udp://... | rtmp://... | srt://... | tcp://...]"
+  echo "Usage: ./toys/capturecard.sh /dev/videoX hw:1,0 [udp://... | rtmp://... | srt://... | tcp://...] (mpegts|nut|fmp4|flv|...)"
+  echo "If you are using udp:// multicast, it's recommended to use mpegts. Otherwise, nut is recommended."
   exit 1
 fi
 
@@ -15,9 +17,9 @@ ffmpeg \
   -init_hw_device qsv=hw \
   -filter_hw_device hw  \
   -fflags nobuffer -flags low_delay \
-  -thread_queue_size 512 -f v4l2 -input_format mjpeg -framerate 60 -c:v mjpeg_qsv -i ${vid_in} \
-  -thread_queue_size 512 -f alsa -i ${aud_in} \
-  -f mpegts \
+  -thread_queue_size 512 -f v4l2 -input_format mjpeg -framerate 60 -c:v mjpeg_qsv -i "${vid_in}" \
+  -thread_queue_size 512 -f alsa -i "${aud_in}" \
+  -f "${cont}" \
   -c:v hevc_qsv -look_ahead 0 -bf 0 -low_power 1 -vb 5M \
   -c:a libopus -ab 128k \
   "${out}"
