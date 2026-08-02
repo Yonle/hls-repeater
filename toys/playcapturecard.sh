432e96aa2cee5805b16f31c9207d2038d0ea52c6
Yonle <yonle@proton.me>
2026-08-03 03:41:11 +0700

    playcapturecard: add option to change FPS and VIDEO_SIZE
    Signed-off-by: Yonle <yonle@proton.me>

--- toys/playcapturecard.sh
+++ toys/playcapturecard.sh
@@ -7,15 +7,30 @@ if [ -z "$vid_in" ] || [ -z "$aud_in" ]; then
 cat <<-EOF
 Usage: ./toys/playcapturecard.sh <v4l2device> <pipewiresink>
 
+Environment Variables:
+  FPS         : V4L2's MJPEG target FPS (def: unset)
+  VIDEO_SIZE  : V4L2's MJPEG video size (def: unset)
+
 Example:
   ./playcapturecard.sh /dev/video2 alsa_input.usb-MACROSILICON_2109-02.analog-stereo
 
+  or with custom param:
+  env FPS=50 VIDEO_SIZE=1920x1080 ./playcapturecard.sh /dev/video2 alsa_input.usb-MACROSILICON_2109-02.analog-stereo
+
 To get your pipewire sink, Please do the following:
   pw-cli ls | grep -i node.name
+
+When using custom param, Please ensure that the mode that you are looking for is available at:
+  v4l2-ctl -d /dev/videoX --list-formats-ext
 EOF
 
 exit 1
 fi
+
+lavf_flags="input_format=mjpeg"
+
+! [ -z "${FPS}"        ] && lavf_flags+=",framerate=${FPS}"
+! [ -z "${VIDEO_SIZE}" ] && lavf_flags+=",video_size=${VIDEO_SIZE}"
 
 pw-cat \
   --target "${aud_in}" \
@@ -33,7 +48,7 @@ mpv \
   --profile=low-latency \
   --cache=no \
   --demuxer-lavf-format=v4l2 \
-  --demuxer-lavf-o=input_format=mjpeg \
+  --demuxer-lavf-o="${lavf_flags}" \
   --video-sync=display-vdrop \
   --demuxer-max-bytes=64KiB \
   --demuxer-max-back-bytes=0 \
