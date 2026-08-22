e202aa3932a3a406294780037b9fa247d7519b1a
Yonle <yonle@proton.me>
2026-08-23 05:46:00 +0700

    add some hls flags
    Signed-off-by: Yonle <yonle@proton.me>

--- relay.sh
+++ relay.sh
@@ -95,7 +95,7 @@ CMD+=(
 	-hls_start_number_source datetime
 	-hls_time "$HLS_TIME"
 	-hls_list_size "$HLS_LIST_SIZE"
-	-hls_flags delete_segments+append_list+temp_file
+	-hls_flags delete_segments+append_list+temp_file+independent_segments+program_date_time
 	-hls_delete_threshold "$HLS_DELETE_THRESHOLD"
 	-hls_segment_filename "$streamdir/$HLS_SEGMENT_FILENAME"
 )
