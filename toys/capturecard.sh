3574d54b78871f20a5cc3898789c79245c2db891
Yonle <yonle@proton.me>
2026-09-20 03:05:59 +0700

    capturecard: use hevc lmfao
    Signed-off-by: Yonle <yonle@proton.me>

--- toys/capturecard.sh
+++ toys/capturecard.sh
@@ -12,7 +12,7 @@ vbfs="${VIDEO_BUFSIZE:-12M}"
 ab="${AUDIO_BITRATE:-128k}"
 bf="${VIDEO_BF:-3}"
 lookahead="${VIDEO_LOOKAHEAD:-32}"
-vcodec="${VIDEO_CODEC:-h264}"
+vcodec="${VIDEO_CODEC:-hevc}"
 ict="${ICECAST_CONTENT_TYPE:-video/x-matroska}"
 
 if [ -z "$vid_in" ] || [ -z "$aud_in" ] || [ -z "$out" ]; then
